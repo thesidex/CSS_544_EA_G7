29ff7d817bce3262c25e5f9e84740d65f09707f7
thesidex <42481614+thesidex@users.noreply.github.com>
2024-03-16 18:06:04 -0500

    scanner crud implementation

---
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/contract/ScannerPayload.java
@@ -0,0 +1,20 @@
+package edu.miu.cs.cs544.service.contract;
+
+import java.io.Serializable;
+import java.util.Set;
+import edu.miu.cs.cs544.domain.Location;
+import lombok.Data;
+
+@Data
+public class ScannerPayload implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer scannerId;
+	
+	private String location;
+
+	private Set<Location> locations;
+
+    private Set<Record> records;
+}
