c0a891202e589acdc1a2b1e13c129c7b508b1369
SarthakAdhikari <adhikari.sarthak@gmail.com>
2024-03-18 13:11:23 -0500

    Fix CI/CD

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
@@ -8,18 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import edu.miu.cs.cs544.service.MemberService;
 
 @SpringBootApplication(scanBasePackages = {"edu.miu.common", "edu.miu.cs.cs544"})
-public class Application implements CommandLineRunner {
+public class Application  {
 
-	@Autowired
-	private MemberService service;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		service.findAll().forEach(System.out::println);
-	}
+
 
 }
