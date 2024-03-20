44a06d28bfcf6548fd367e15e11f5ab56eb99d30
thesidex <42481614+thesidex@users.noreply.github.com>
2024-03-19 21:41:21 -0500

    update env

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
@@ -1,11 +1,7 @@
 package edu.miu.cs.cs544;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import edu.miu.cs.cs544.service.MemberService;
 
 @SpringBootApplication(scanBasePackages = {"edu.miu.common", "edu.miu.cs.cs544"})
 public class Application  {
@@ -13,7 +9,4 @@ public class Application  {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-
-
 }
