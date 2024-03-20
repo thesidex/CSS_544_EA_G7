daf04ca6e9f456ebccdbb9d2084eda8e48cb71d5
thesidex <42481614+thesidex@users.noreply.github.com>
2024-03-19 20:47:22 -0500

    Attendance implementation

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/AccountService.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/AccountService.java
@@ -2,14 +2,13 @@ package edu.miu.cs.cs544.service;
 
 import edu.miu.common.service.BaseReadWriteService;
 import edu.miu.cs.cs544.domain.Account;
-import edu.miu.cs.cs544.domain.Member;
 import edu.miu.cs.cs544.service.contract.AccountPayload;
-import edu.miu.cs.cs544.service.contract.MemberPayload;
+import edu.miu.cs.cs544.service.contract.AttendancePayload;
+import java.util.List;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
 @Component
 public interface AccountService extends BaseReadWriteService<AccountPayload, Account, Long> {
-
-
-
+	List<String> findAccountsByBalanceCondition();
+	List<AttendancePayload> getAttendanceByAccountIdAndStartTimeAndEndTime(Long accountId, String startTime, String endTime);
 }
---
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/service/mapper/AttendanceToAttendancePayloadMapper.java
@@ -0,0 +1,15 @@
+package edu.miu.cs.cs544.service.mapper;
+
+import edu.miu.common.service.mapper.BaseMapper;
+import edu.miu.cs.cs544.domain.Attendance;
+import edu.miu.cs.cs544.service.contract.AttendancePayload;
+import ma.glasnost.orika.MapperFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendanceToAttendancePayloadMapper extends BaseMapper<Attendance, AttendancePayload> {
+
+    public AttendanceToAttendancePayloadMapper(MapperFactory mapperFactory) {
+        super(mapperFactory, Attendance.class, AttendancePayload.class);
+    }
+}
