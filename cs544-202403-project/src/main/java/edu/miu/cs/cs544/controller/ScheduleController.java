3579a44ea2c2536ce85185fc33c59fcfcd6a7729
PranjalSharma13 <1SI17CS077@sit.ac.in>
2024-03-21 10:05:44 -0500

    corrected table cascade

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/ScheduleController.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/controller/ScheduleController.java
@@ -3,11 +3,25 @@ package edu.miu.cs.cs544.controller;
 
 import edu.miu.common.controller.BaseReadWriteController;
 import edu.miu.cs.cs544.domain.Schedule;
+import edu.miu.cs.cs544.service.ScheduleService;
 import edu.miu.cs.cs544.service.contract.SchedulePayload;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/schedules")
 public class ScheduleController  extends BaseReadWriteController<SchedulePayload, Schedule, Long> {
+    @Autowired
+    private ScheduleService scheduleService;
+    @PostMapping("/add/{eventId}")
+    public ResponseEntity<?> addScheduleForEvent(@PathVariable Long eventId, @RequestBody SchedulePayload schedulePayload) {
+        try {
+            Schedule createdSchedule = scheduleService.createScheduleForEvent(eventId, schedulePayload);
+            return new ResponseEntity<>(createdSchedule, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
